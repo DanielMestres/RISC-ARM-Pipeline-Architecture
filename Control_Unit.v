db9ff7100875c6e12a06415783aaac8f211d6288
Daniel <daniel.mestres@upr.edu>
2022-11-15 14:22:48 -0400

    progress on control unit

--- Control_Unit.v
+++ Control_Unit.v
@@ -21,6 +21,8 @@ always @(IR_in) begin
                 RF_ID_out = 1'b0;
                 B_ID_out = 1'b0;
                 size_out = 2'b00;
+                R_W_out = 1'b0;
+                B_L_out = 1'b0;
             end
             3'b001: begin       // Data Processing Immediate
                 opcode_out = IR_in[24:21];
@@ -29,20 +31,22 @@ always @(IR_in) begin
                 RF_ID_out = 1'b0;
                 B_ID_out = 1'b0;
                 size_out = 2'b00;
+                R_W_out = 1'b0;
+                B_L_out = 1'b0;
             end
             3'b010: begin       // Load/Store Immediate Offset
                 opcode_out = 4'b0000;
                 SE_ID_out = 1'b0;
                 LI_ID_out = 1'b1;
                 RF_ID_out = 1'b0;
-                B_ID_out = 1'b0;
+                B_ID_out = 1'b0;    // ADD RW AND SIZE (B byte ??)
             end
             3'b011: begin       // Load/Store Register Offset
                 opcode_out = 4'b000;
                 SE_ID_out = 1'b0;
                 LI_ID_out = 1'b1;
-                RF_ID_out = 1'b0;
-                B_ID_out = 1'b0;
+                RF_ID_out = 1'b1;
+                B_ID_out = 1'b0;    // ADD RW AND SIZE (B byte ??)
             end
             3'b101: begin       // Branch & Link
                 opcode_out = 4'b000;
@@ -50,6 +54,9 @@ always @(IR_in) begin
                 LI_ID_out = 1'b0;
                 RF_ID_out = 1'b0;
                 B_ID_out = 1'b1;
+                if(IR_in[24]) begin // Check for linking
+                    B_L_out = 1'b1;
+                end
             end
         endcase
     end else begin              // Nop
@@ -58,7 +65,9 @@ always @(IR_in) begin
         LI_ID_out = 1'b0;
         RF_ID_out = 1'b0;
         B_ID_out = 1'b0;
+        size_out = 2'b00;
         R_W_out = 1'b0;
+        B_L_out = 1'b0;
     end
 end
 endmodule
